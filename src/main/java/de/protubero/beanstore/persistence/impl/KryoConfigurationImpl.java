bce8bba5ac4d84ecc39e68c8cc92ca9827993acd
Manfred Schäfer <4920713+protubero@users.noreply.github.com>
2023-12-22 15:35:11 +0100

    things

--- src/main/java/de/protubero/beanstore/persistence/impl/KryoConfigurationImpl.java
+++ src/main/java/de/protubero/beanstore/persistence/impl/KryoConfigurationImpl.java
@@ -1,6 +1,8 @@
 package de.protubero.beanstore.persistence.impl;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,7 +10,9 @@ import org.slf4j.LoggerFactory;
 import com.esotericsoftware.kryo.kryo5.Kryo;
 import com.esotericsoftware.kryo.kryo5.Registration;
 import com.esotericsoftware.kryo.kryo5.Serializer;
+import com.esotericsoftware.kryo.kryo5.serializers.TimeSerializers.DurationSerializer;
 import com.esotericsoftware.kryo.kryo5.serializers.TimeSerializers.InstantSerializer;
+import com.esotericsoftware.kryo.kryo5.serializers.TimeSerializers.LocalDateTimeSerializer;
 
 import de.protubero.beanstore.persistence.api.KryoConfiguration;
 import de.protubero.beanstore.persistence.api.KryoId;
@@ -33,6 +37,24 @@ public class KryoConfigurationImpl implements KryoConfiguration {
 //		kryo.register(PersistentProperty.class, 24);
 		kryo.register(Instant.class, new InstantSerializer(), 25);
 //		kryo.register(PersistentBean.class, new PersistentBeanSerializer(), 26);
+		
+		kryo.register(Duration.class, new DurationSerializer(), 26);
+		kryo.register(LocalDateTime.class, new LocalDateTimeSerializer(), 27);
+//		if (isClassAvailable("java.time.LocalDate")) kryo.addDefaultSerializer(LocalDate.class, LocalDateSerializer.class);
+//		if (isClassAvailable("java.time.LocalTime")) kryo.addDefaultSerializer(LocalTime.class, LocalTimeSerializer.class);
+//		if (isClassAvailable("java.time.LocalDateTime"))
+//			kryo.addDefaultSerializer(LocalDateTime.class, LocalDateTimeSerializer.class);
+//		if (isClassAvailable("java.time.ZoneOffset")) kryo.addDefaultSerializer(ZoneOffset.class, ZoneOffsetSerializer.class);
+//		if (isClassAvailable("java.time.ZoneId")) kryo.addDefaultSerializer(ZoneId.class, ZoneIdSerializer.class);
+//		if (isClassAvailable("java.time.OffsetTime")) kryo.addDefaultSerializer(OffsetTime.class, OffsetTimeSerializer.class);
+//		if (isClassAvailable("java.time.OffsetDateTime"))
+//			kryo.addDefaultSerializer(OffsetDateTime.class, OffsetDateTimeSerializer.class);
+//		if (isClassAvailable("java.time.ZonedDateTime"))
+//			kryo.addDefaultSerializer(ZonedDateTime.class, ZonedDateTimeSerializer.class);
+//		if (isClassAvailable("java.time.Year")) kryo.addDefaultSerializer(Year.class, YearSerializer.class);
+//		if (isClassAvailable("java.time.YearMonth")) kryo.addDefaultSerializer(YearMonth.class, YearMonthSerializer.class);
+//		if (isClassAvailable("java.time.MonthDay")) kryo.addDefaultSerializer(MonthDay.class, MonthDaySerializer.class);
+//		if (isClassAvailable("java.time.Period")) kryo.addDefaultSerializer(Period.class, PeriodSerializer.class);		
 		
 	}
 	
